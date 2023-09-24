752e50ae95422545b8c62d5de43e27eb7019e269
MikitaMastserau <nik.redko@gmail.com>
2023-09-24 16:47:08 +0700

    add reviews, workers

---
+++ src/pages/Home/components/Reviews/index.jsx
@@ -0,0 +1,82 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+
+import { Supertitle } from "components/Supertitle";
+
+import photo1 from "static/images/photo1.png";
+import photo2 from "static/images/photo2.png";
+import photo3 from "static/images/photo3.png";
+import photo4 from "static/images/photo4.jpg";
+import photo5 from "static/images/photo5.jpg";
+
+import "swiper/scss";
+import "swiper/scss/pagination";
+import "./styles.scss";
+
+export const Reviews = () => {
+   return (
+      <section className="reviews">
+         <div className="container">
+            <div className="reviews__hat">
+               <Supertitle color="#171B1B" text="Our Baker" />
+               <h2>Meet Our Professional Baker</h2>
+            </div>
+            <div className="reviews__slider">
+               <Swiper
+                  grabCursor={true}
+                  slidesPerView={3}
+                  pagination={{
+                     clickable: true,
+                  }}
+                  centeredSlides={true}
+                  initialSlide={1}
+                  slideToClickedSlide={true}
+                  modules={[Pagination]}
+                  className="reviewsSwiper"
+               >
+                  <SwiperSlide>
+                     <div className="reviewsPhotos">
+                        <div className="stars"></div>
+                        <img src={photo1} alt="" />
+                     </div>
+                     <h4 className="reviewsTitle">Very, Very Delicious</h4>
+                     <p className="reviewsText">"Nothing the copy said could convince her and so it didn’t take long. A small river named Duden flows by their"</p>
+                  </SwiperSlide>
+                  <SwiperSlide>
+                     <div className="reviewsPhotos">
+                        <div className="stars"></div>
+                        <img src={photo2} alt="" />
+                     </div>
+                     <h4 className="reviewsTitle">Best Bakery in Town</h4>
+                     <p className="reviewsText">"Copy Writers ambushed her, made her drunk with Longe and Parole. A small river named Duden flows by their place"</p>
+                  </SwiperSlide>
+                  <SwiperSlide>
+                     <div className="reviewsPhotos">
+                        <div className="stars"></div>
+                        <img src={photo3} alt="" />
+                     </div>
+                     <h4 className="reviewsTitle">Well Organized</h4>
+                     <p className="reviewsText">«And if she hasn’t been rewritten, then they are still using her. A small river named Duden flows by their place"</p>
+                  </SwiperSlide>
+                  <SwiperSlide>
+                     <div className="reviewsPhotos">
+                        <div className="stars"></div>
+                        <img src={photo4} alt="" />
+                     </div>
+                     <h4 className="reviewsTitle">Tasty like grandma's</h4>
+                     <p className="reviewsText">"Blue and purple clouds rushed over the earth. Pigeons in the squares were busy in the treetops dropping twigs."</p>
+                  </SwiperSlide>
+                  <SwiperSlide>
+                     <div className="reviewsPhotos">
+                        <div className="stars"></div>
+                        <img src={photo5} alt="" />
+                     </div>
+                     <h4 className="reviewsTitle">Good service</h4>
+                     <p className="reviewsText">"The peasants looked at the fields with alarm. The spring was uneven, the weather changed constantly."</p>
+                  </SwiperSlide>
+               </Swiper>
+            </div>
+         </div>
+      </section>
+   )
+};
