f2637973b036dc588b26901a4a990bcf2e1e618d
MikitaMastserau <nik.redko@gmail.com>
2023-09-26 00:19:01 +0700

    add responsive

--- src/pages/Home/components/About/index.jsx
+++ src/pages/Home/components/About/index.jsx
@@ -41,9 +41,9 @@ export const About = () => {
                      </SwiperSlide>
                      <SwiperSlide>
                         <Supertitle color="#171B1B" text="02" />
-                        <h4>The spring was uneven</h4>
-                        <p>The weather changed constantly, blue and purple clouds rushed over the earth. The peasants looked at the fields with alarm.</p>
-                        <div>
+                        <h4 className="sliderTitle">The spring was uneven</h4>
+                        <p className="sliderText">The weather changed constantly, blue and purple clouds rushed over the earth. The peasants looked at the fields with alarm.</p>
+                        <div className="sliderDiv">
                            <p>In London</p>
                            <p>Opened umbrellas</p>
                            <p>Weather in April</p>
@@ -51,9 +51,9 @@ export const About = () => {
                      </SwiperSlide>
                      <SwiperSlide>
                         <Supertitle color="#171B1B" text="03" />
-                        <h4>Then Colonel Abel</h4>
-                        <p>Suddenly the youngest and most dapper of the three leaned forward. Yesterday he had lunch. Here the speaker lowered his voice.</p>
-                        <div>
+                        <h4 className="sliderTitle">Then Colonel Abel</h4>
+                        <p className="sliderText">Suddenly the youngest and most dapper of the three leaned forward. Yesterday he had lunch. Speaker lowered his voice.</p>
+                        <div className="sliderDiv">
                            <p>With a short wave</p>
                            <p>Sat back in chair</p>
                            <p>Gray heads</p>
@@ -61,9 +61,9 @@ export const About = () => {
                      </SwiperSlide>
                      <SwiperSlide>
                         <Supertitle color="#171B1B" text="04" />
-                        <h4>The spark of curiosity</h4>
-                        <p>A certain thought came to him, and the words of those around him lost all interest for him. Moreover, they began to irritate him.</p>
-                        <div>
+                        <h4 className="sliderTitle">The spark of curiosity</h4>
+                        <p className="sliderText">A certain thought came to him, and the words of those around him lost all interest for him. Moreover, they began to irritate him.</p>
+                        <div className="sliderDiv">
                            <p>Major Elkins</p>
                            <p>Kindled in the eyes</p>
                            <p>Story now completely</p>
--- src/pages/Home/components/Main/index.jsx
+++ src/pages/Home/components/Main/index.jsx
@@ -1,15 +1,20 @@
 import { Supertitle } from "components/Supertitle";
 
+import mainBread from "static/images/mainBread.jpg";
+
 import styles from "./styles.module.scss";
 
 export const Main = () => {
    return (
       <main className={styles.main}>
          <div className={styles.container}>
-            <Supertitle color="#000" text="Since 1984" />
-            <h1>Fresh Bakery Every Day</h1>
-            <p className={styles.main__text}>Even the all-powerful Pointing has no control about the blind texts it is an almost.</p>
+            <div className={styles.main__hat}>
+               <Supertitle color="#000" text="Since 1984" />
+               <h1>Fresh Bakery Every Day</h1>
+               <p className={styles.main__text}>Even the all-powerful Pointing has no control about the blind texts it is an almost.</p>
+            </div>
          </div>
+         <img className={styles.main__photo} src={mainBread} alt="" />
       </main>
    )
 };
